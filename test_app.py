88a0689fddebeda58cb583e4676c5b49f60f0442
kallu136 <kalluchiya532@gmail.com>
2024-12-20 11:29:49 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    def test_hello(self):
+    def test_hi(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), "Hello, World!")
